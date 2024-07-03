22afcc0f92db6e570d676ef6d5f76f906910761d
Sidney Sousa <sidney.sousa@ifms.edu.br>
2024-07-02 20:31:40 -0400

    criando testes

---
+++ revisao/src/components/SentenceInput/index.cy.tsx
@@ -0,0 +1,9 @@
+import SentenceInput from ".";
+
+describe("<SentenceInput />", () => {
+  it("should keep user text", () => {
+    const sentence = "Fala, galera!";
+    const setSentence = () => {};
+    cy.mount(<SentenceInput sentence={sentence} setSentence={setSentence} />);
+  });
+});
---
+++ revisao/src/components/SentenceInput/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+type Props = {
+  sentence: string;
+  setSentence: (newState: string) => void;
+};
+
+const SentenceInput = ({ sentence, setSentence }: Props) => {
+  return (
+    <div className={styles.container}>
+      <input
+        type="text"
+        value={sentence}
+        onChange={(e) => setSentence(e.target.value)}
+        placeholder="Digite a frase"
+      />
+    </div>
+  );
+};
+
+export default SentenceInput;
