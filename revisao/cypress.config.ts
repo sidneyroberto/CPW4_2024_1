2b850c45cd703cb3b1b9aa5ce240dcdb86c2c436
Sidney Sousa <sidney.sousa@ifms.edu.br>
2024-07-23 19:19:54 -0400

    iniciando teste e2e

--- revisao/cypress.config.ts
+++ revisao/cypress.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
       bundler: "webpack",
     },
   },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+  },
 });
--- revisao/src/App.tsx
+++ revisao/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import styles from "./App.module.css";
+import SentenceInput from "./components/SentenceInput";
+import CounterPanel from "./components/CounterPanel";
+
+const App = () => {
+  const [sentence, setSentence] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <SentenceInput
+        sentence={sentence}
+        setSentence={setSentence}
+      ></SentenceInput>
+
+      <CounterPanel sentence={sentence} />
     </div>
   );
-}
+};
 
 export default App;
