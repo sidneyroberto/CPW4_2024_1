51b34cd99cdd43d6f3720e54b860ccdcc570f3ce
Sidney Sousa <sidney.sousa@ifms.edu.br>
2024-07-31 19:15:34 -0400

    criando comando

---
+++ exemplo02/cypress/e2e/details.cy.ts
@@ -0,0 +1,8 @@
+describe("e2e tests over details page", () => {
+  beforeEach(() => {
+    cy.visit("");
+    cy.fixture("apiResponse").as("apiResponse");
+  });
+
+  it("should show correct meanings about a word", function () {});
+});
